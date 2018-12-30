c36f834ea89b4cb9fbe97030de670e47f6fcc11f
VeggieBenji <46262972+VeggieBenji@users.noreply.github.com>
2018-12-30 15:32:26 +0100

    mise du jeu dans le depot
    Ajout des fichiers du jeu

---
+++ offline.js
@@ -0,0 +1,30 @@
+﻿{
+	"version": 1546177921,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/mom-sheet0.png",
+		"images/kiwi-sheet0.png",
+		"images/kiwi-sheet1.png",
+		"images/kiwi-sheet2.png",
+		"images/grandma-sheet0.png",
+		"images/bro-sheet0.png",
+		"images/bg-sheet0.png",
+		"images/sprite-sheet0.png",
+		"images/spritefont.png",
+		"images/me-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"media/blip_select.m4a",
+		"media/blip_select.ogg",
+		"media/nature sketch.m4a",
+		"media/nature sketch.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
